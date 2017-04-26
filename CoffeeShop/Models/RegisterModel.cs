8a377187ebcb7346fc7a66cf3b3ac954c908bf0e
Pete Rossman <pete.rossman@gmail.com>
2017-04-26 14:58:35 -0400

    Add password to RegisterModel.cs

--- CoffeeShop/Models/RegisterModel.cs
+++ CoffeeShop/Models/RegisterModel.cs
@@ -13,5 +13,7 @@ namespace CoffeeShop.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
     }
 }
