076565be9ff59c10b5de4125bdc9eb6ebbe2a134
Pete Rossman <pete.rossman@gmail.com>
2017-04-26 11:52:14 -0400

    Add all other files except Readme

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Items()
+        {
+            ViewBag.Message = "These are items!";
+            return View();
+        }
+
+        public ActionResult Success(RegisterModel r)
+        {                     
+            ViewBag.Message = " Your first name is " + r.FirstName + " and your email address is " + r.EmailAddress;
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "My name is Pete Rossman, and I'm currently a student in Grand Circus' .NET(C#) Bootcamp in Grand Rapids.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This Coffee Shop is a project made by Pete Rossman for Grand Circus' .NET(C#) Bootcamp.";
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register with GC Coffee";
+            return View();
+        }
+    }
+}
---
+++ CoffeeShop/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class RegisterModel
+    {
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
